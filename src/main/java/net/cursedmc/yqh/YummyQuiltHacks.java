3c16fcbed645365e3849610b63d1f797215e7f5d
TehcJS <tehcdev@gmail.com>
2022-07-01 14:27:45 -0400

    we don't need a language adapter

--- src/main/java/net/cursedmc/yqh/YummyQuiltHacks.java
+++ src/main/java/net/cursedmc/yqh/YummyQuiltHacks.java
@@ -2,14 +2,9 @@ package net.cursedmc.yqh;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.quiltmc.loader.api.LanguageAdapter;
-import org.quiltmc.loader.api.ModContainer;
 
-public class YummyQuiltHacks implements LanguageAdapter {
+public class YummyQuiltHacks {
 	private static final Logger LOGGER = LogManager.getLogger("YummyQuiltHacks");
-	
-	@Override
-	public native <T> T create(ModContainer mod, String value, Class<T> type);
 	
 	static {
 		LOGGER.warn("Quilt has been successfully pwned >:3");
